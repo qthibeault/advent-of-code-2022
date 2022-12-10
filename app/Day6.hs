99c76bbb36aa0f329d10a114a06c0fa18d28324f
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-12-10 12:11:43 -0700

    Solution for day 6

--- app/Day6.hs
+++ app/Day6.hs
@@ -1,4 +1,18 @@
 module Day6 where
 
+findMessageOffset :: Int -> String -> Int
+findMessageOffset prefixLen = go 0 []
+    where
+        go _ _ [] = error "No prefix found"
+        go n prefix (c:rest)
+            | length prefix == prefixLen = n
+            | otherwise  = let prefix' = takeWhile (c /=) prefix in go (n + 1) (c:prefix') rest
+
+readDatastream :: IO String
+readDatastream = head . lines <$> readFile "data/day6.txt"
+
 main :: IO ()
-main = putStrLn "Day 6"
+main = do
+    datastream <- readDatastream
+    print (findMessageOffset 4 datastream)
+    print (findMessageOffset 14 datastream)
